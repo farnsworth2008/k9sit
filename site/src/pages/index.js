224ad591c0be849a3c8db546bb9f3428a6718e2b
Mark Farnsworth <farnsworth2008@gmail.com>
2023-02-13 12:56:17 -0500

    1.0.0 Updated Version

--- site/src/pages/index.js
+++ site/src/pages/index.js
@@ -6,7 +6,7 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 import styles from './index.module.css';
 
-const version = '1.0.4';
+const version = '1.0.6';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
