58c25ae2a2c7044dc94f9f3c62f2fe5cecfc111f
Mark Farnsworth <farnsworth2008@gmail.com>
2023-03-16 23:49:56 -0400

    1.208.0 Updated version

--- terraform.tf
+++ terraform.tf
@@ -1,8 +1,8 @@
 terraform {
-  required_version = ">=1.3.9"
+  required_version = ">=1.4.2"
 
   required_providers {
-    aws      = ">=4.57.1"
+    aws      = ">=4.59.0"
     external = ">=2.3.1"
     random   = ">=3.4.3"
     time     = ">=0.9.1"
